b2ae6fb1400e7803765685ec0646ba9b10cf6965
uknable <uknable@hotmail.com>
2017-02-18 12:27:51 +1100

    added sort buttons

--- app/controllers/champion_controller.rb
+++ app/controllers/champion_controller.rb
@@ -1,5 +1,13 @@
 class ChampionController < ApplicationController
   def index
-  	@champion = Champion.order(:name)
+  	@champion = Champion.order(params[:champion_category])
   end
+  
+  def show
+  	@champion = Champion.all
+  end
+=begin
+  def sort(category)
+  	@champion = Champion.order(category)
+=end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'champion/index'
-
+	resources :champion, only: [:index, :show]
+  root 'champion#index'
 end
