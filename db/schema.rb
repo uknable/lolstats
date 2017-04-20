98b5460e01606b1f98e02495986bdb13f887384d
uknable <uknable@hotmail.com>
2017-04-20 19:20:45 +1000

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170228033746) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "champions", force: :cascade do |t|
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
